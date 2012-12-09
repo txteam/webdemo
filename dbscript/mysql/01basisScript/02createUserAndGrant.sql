7d1fca4aca9407f086bb5afb0f4d04ee053cf712
240638006 <240638006@qq.com>
2012-12-09 20:11:07 +0800

    xxxx

--- dbscript/mysql/01basisScript/02createUserAndGrant.sql
+++
@@ -1,6 +0,0 @@
---****************************************************************************
---create user and grant
---****************************************************************************
-GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP,ALTER ON 
-	db_webdemo.* TO db_webdemo@localhost IDENTIFIED BY 'db_webdemo';
-SET PASSWORD FOR 'db_webdemo'@'localhost' = OLD_PASSWORD('db_webdemo');
