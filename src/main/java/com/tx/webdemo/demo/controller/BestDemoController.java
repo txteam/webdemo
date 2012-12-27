ab00e685e79f38051e8a0e2ed34fa189d875284b
240638006 <240638006@qq.com>
2012-12-27 18:00:13 +0800

    xxx

--- src/main/java/com/tx/webdemo/demo/controller/BestDemoController.java
+++ src/main/java/com/tx/webdemo/demo/controller/BestDemoController.java
@@ -6,8 +6,6 @@
  */
 package com.tx.webdemo.demo.controller;
 
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,7 +15,6 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang.math.NumberUtils;
-import org.apache.commons.lang.time.DateFormatUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -27,54 +24,21 @@ import com.tx.core.paged.model.PagedList;
 import com.tx.webdemo.demo.model.Demo;
 import com.tx.webdemo.demo.service.DemoService;
 
-
- /**
-  * 最佳实践参考
-  * <功能详细描述>
-  * 
-  * @author  PengQingyang
-  * @version  [版本号, 2012-12-22]
-  * @see  [相关类/方法]
-  * @since  [产品/模块版本]
-  */
-@Controller("bestDemoController")
+/**
+ * 最佳实践参考
+ * <功能详细描述>
+ * 
+ * @author  PengQingyang
+ * @version  [版本号, 2012-12-22]
+ * @see  [相关类/方法]
+ * @since  [产品/模块版本]
+ */
+@Controller("bestDemoController")   
 @RequestMapping("/bestDemo")
 public class BestDemoController {
     
-    @Resource(name="demoService")
+    @Resource(name = "demoService")
     private DemoService demoService;
-    
-    /**
-      * 出发批量插入demo<br/>
-      * 为测试准备测试数据<br/>
-      * 这里数据没有从界面上去获取<br/>
-      * 批量插入的业务，一般来说会在业务层对批量方法结果进行处理<br/>
-      * <功能详细描述> [参数说明]
-      * 
-      * @return void [返回类型说明]
-      * @exception throws [异常类型] [异常说明]
-      * @see [类、类#方法、类#成员]
-     */
-    @RequestMapping("/batchInsertDemoForUse")
-    @ResponseBody()
-    public boolean batchInsertDemoForUse(){
-        List<Demo> demoList = new ArrayList<Demo>();
-        String loginName = "testBatchInserDemoNonStop"
-                + DateFormatUtils.format(new Date(), "yyyyMMddHHmmssSSS");
-
-        for (int i = 0; i < 100; i++) {
-            Demo newDemo = new Demo();
-            newDemo.setName("testName");
-            newDemo.setLoginName(loginName + (int) (Math.random() * 10000000));
-            newDemo.setPassword("test");
-            newDemo.setCreateDate(new Date());
-            newDemo.setLastUpdateDate(new Date());
-            demoList.add(newDemo);
-        }
-        
-        this.demoService.batchInsertDemoNotStopWhenException(demoList,false);  
-        return true;
-    }
     
     /**
       * 查询demo列表<br/>
@@ -87,7 +51,7 @@ public class BestDemoController {
       * @see [类、类#方法、类#成员]
      */
     @RequestMapping("/queryDemoList")
-    public String queryDemoList(HashMap<String, Object> params){
+    public String queryDemoList(HashMap<String, Object> params) {
         
         List<Demo> demoList = this.demoService.queryDemoList();
         
@@ -96,7 +60,6 @@ public class BestDemoController {
     }
     
     /**
-     * 
       *<功能简述>
       *<功能详细描述>
       * @param params
@@ -127,7 +90,7 @@ public class BestDemoController {
      */
     @RequestMapping("/ajax/ajaxQueryDemoList")
     @ResponseBody()
-    public List<Demo> ajaxQueryDemoList(HashMap<String, Object> params){
+    public List<Demo> ajaxQueryDemoList(HashMap<String, Object> params) {
         List<Demo> demoList = this.demoService.queryDemoList();
         
         return demoList;
@@ -144,18 +107,18 @@ public class BestDemoController {
      */
     @RequestMapping("/ajax/ajaxQueryDemoPagedList")
     @ResponseBody()
-    public PagedList<Demo> ajaxQueryDemoPagedList(){
+    public PagedList<Demo> ajaxQueryDemoPagedList() {
         HttpServletRequest request = WebContextUtils.getRequest();
-        int pageIndex = NumberUtils.toInt(request.getParameter("pageIndex"),1);
-        int pageSize = NumberUtils.toInt(request.getParameter("pageSize"),10);
+        int pageIndex = NumberUtils.toInt(request.getParameter("pageIndex"), 1);
+        int pageSize = NumberUtils.toInt(request.getParameter("pageSize"), 10);
         Map<String, String[]> paramMap = request.getParameterMap();
-        for(Entry<String,String[]> entry : paramMap.entrySet()){
+        for (Entry<String, String[]> entry : paramMap.entrySet()) {
             System.out.println(entry.getKey());
         }
-        PagedList<Demo> demoPagedList = this.demoService.queryDemoPagedList(pageIndex, pageSize);
+        PagedList<Demo> demoPagedList = this.demoService.queryDemoPagedList(pageIndex,
+                pageSize);
         
         return demoPagedList;
     }
-    
     
 }
--- src/main/java/com/tx/webdemo/demo/controller/DemoController.java
+++ src/main/java/com/tx/webdemo/demo/controller/DemoController.java
@@ -6,16 +6,25 @@
  */
 package com.tx.webdemo.demo.controller;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Resource;
 
+import org.apache.commons.beanutils.PropertyUtils;
+import org.apache.commons.collections.MapUtils;
+import org.apache.commons.lang.time.DateFormatUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.tx.component.mainframe.config.MenuItemConfig;
+import com.tx.component.mainframe.model.DefaultMenuItem;
 import com.tx.component.mainframe.model.MenuItem;
 import com.tx.component.mainframe.service.MenuService;
 import com.tx.core.paged.model.PagedList;
@@ -36,6 +45,7 @@ import com.tx.webdemo.demo.service.DemoService;
 @RequestMapping(value = "/demo")
 public class DemoController {
     
+    @SuppressWarnings("unused")
     private static Logger logger = LoggerFactory.getLogger(DemoController.class);
     
     @Resource(name = "demoService")
@@ -43,6 +53,38 @@ public class DemoController {
     
     @Resource(name= "menuService")
     private MenuService menuService;
+    
+    /**
+     * 出发批量插入demo<br/>
+     * 为测试准备测试数据<br/>
+     * 这里数据没有从界面上去获取<br/>
+     * 批量插入的业务，一般来说会在业务层对批量方法结果进行处理<br/>
+     * <功能详细描述> [参数说明]
+     * 
+     * @return void [返回类型说明]
+     * @exception throws [异常类型] [异常说明]
+     * @see [类、类#方法、类#成员]
+    */
+   @RequestMapping("/batchInsertDemoForUse")
+   @ResponseBody()
+   public boolean batchInsertDemoForUse(){
+       List<Demo> demoList = new ArrayList<Demo>();
+       String loginName = "testBatchInserDemoNonStop"
+               + DateFormatUtils.format(new Date(), "yyyyMMddHHmmssSSS");
+
+       for (int i = 0; i < 100; i++) {
+           Demo newDemo = new Demo();
+           newDemo.setName("testName");
+           newDemo.setLoginName(loginName + (int) (Math.random() * 10000000));
+           newDemo.setPassword("test");
+           newDemo.setCreateDate(new Date());
+           newDemo.setLastUpdateDate(new Date());
+           demoList.add(newDemo);
+       }
+       
+       this.demoService.batchInsertDemoNotStopWhenException(demoList,false);  
+       return true;
+   }
     
     /**
       * 树demo
@@ -103,4 +145,34 @@ public class DemoController {
         return "/demo/demoPagedList";
     }
     
+    public static void main(String[] args) throws Exception{
+        MenuItemConfig mic = new MenuItemConfig();
+        mic.setId("test");
+        mic.setAuthKey("authKey1,authKey2");
+        mic.setChilds(new ArrayList<MenuItemConfig>());
+        mic.getChilds().add(new MenuItemConfig());
+        mic.getChilds().get(0).setId("testChild0");
+        
+        DefaultMenuItem dmi = new DefaultMenuItem();
+        
+        PropertyUtils.copyProperties(dmi, mic);
+        
+        System.out.println(dmi.getId());
+        
+        Map<String, Object> test = PropertyUtils.describe(dmi);
+        System.out.println();
+        
+        MapUtils.verbosePrint(System.out, "map : ", test);
+        
+        System.out.println(PropertyUtils.getIndexedProperty(dmi, "childs",0));
+        
+        System.out.println(PropertyUtils.getSimpleProperty(dmi, "id"));
+        
+        //System.out.println(PropertyUtils.getMappedProperty(dmi, "sub.id"));
+        
+        System.out.println(PropertyUtils.getNestedProperty(dmi, "childs[0].id"));
+        
+        System.out.println(PropertyUtils.getProperty(dmi, "childs[0].id"));
+    }
+   
 }
